a260fbbcd8193a1b5ed68efe8c7c3856985645db
estebanfer <estebanfer2004@gmail.com>
2022-04-03 02:09:41 -0300

    Many changes - Fixed inverse gravity liquid rooms on non-inverse and viceversa - Fixed dual rooms being on duat or CO - Removed unused entrance_drop from duat that was blocking path - changed SC exit_notop to only be on SC levels to prevent blocked paths

--- getRoomTemplates.py
+++ getRoomTemplates.py
@@ -1,12 +1,13 @@
 from os import mkdir, listdir
 from os.path import isfile
+from enum import IntEnum
 import re
 mypath = "./Created/"
 reTemplates = re.compile(r"\\\.([^\s\n/]*).*\n\/*\n\n([\s\S]*?)(?:(?:\/\/\/)|$)")#(r"\\\.([^\s\n/]*).*\n\/*\n[\s\S]*?(?:(?:\/\/\/)|$)")
-reRooms = re.compile(r"((?:\\!.*\n)*)\n*([\s\S]*?)\n(?:\n|$)")#(r"(\\!.*\n)*\n*([\s\S]*?)\n(?:\n|$)")#(r"\/\/ room(?:\n\\!.*)*\n*([\s\S]*?)\n(?:\n|$)")
-templates = {}
+reRooms = re.compile(r"\n*((?:\\!.*\n)*)\n*([\s\S]*?)\n(?:\n|$)")#(r"(\\!.*\n)*\n*([\s\S]*?)\n(?:\n|$)")#(r"\/\/ room(?:\n\\!.*)*\n*([\s\S]*?)\n(?:\n|$)")
+templates = []
 templatesAllStr = {}
-levels = {}
+filesDict = {}
 toChangeTemplates = [
     "entrance",
     "entrance_drop",
@@ -29,6 +30,19 @@ toChangeTemplates = [
     "chunk_air",
     "chunk_door"
 ]
+sunkenExitsNotop = []
+
+class ROOM_TYPE(IntEnum):
+    NORMAL = 0
+    DUAL = 1
+    LIQUID = 2
+    INVERSE_LIQUID = 3
+
+for n in range(4):
+    templates.append({})
+    for template in toChangeTemplates:
+        templates[n][template] = []
+
 templateSeparator = "////////////////////////////////////////////////////////////////////////////////"
 
 def tryMakeDir(str):
@@ -37,25 +51,34 @@ def tryMakeDir(str):
     except OSError as error:
         print(error)
 
-def getTemplateRooms(level):
+def getTemplateRooms(level, filename):
+    liquidRoomType = ROOM_TYPE.INVERSE_LIQUID if "sunken" in filename or filename == "hundun.lvl" else ROOM_TYPE.LIQUID
     templateMatches = reTemplates.finditer(level)
     for templateMatch in templateMatches:
         templateName = templateMatch.group(1)
         templateRooms = templateMatch.group(2)
+        if not templateName in toChangeTemplates or (
+            filename == "duat.lvl" and templateName == "entrance_drop"):
+                continue
         print(templateName)
         if templateRooms[-1] != "\n":
             templateRooms += "\n"
         roomMatches = reRooms.finditer(templateRooms)
-        for roomMatch in roomMatches:
-            #print(roomMatch)
-            if roomMatch.group(1):
-                roomStr = roomMatch.group(1) + roomMatch.group(2)
-            else:
-                roomStr = roomMatch.group(2)
-            if templateName in templates:
-                templates[templateName].append(roomStr)
-            else:
-                templates[templateName] = [roomStr]
+        if templateName == "exit_notop" and (filename == "sunkencityarea.lvl" or filename == "hundun.lvl"):
+            for roomMatch in roomMatches:
+                room = roomMatch.group(1) + roomMatch.group(2)
+                sunkenExitsNotop.append(room)
+        else:
+            for roomMatch in roomMatches:
+                #print(roomMatch)
+                tags, room = roomMatch.group(1), roomMatch.group(2)
+                room = tags + room
+                if "!liquid" in tags:
+                    templates[liquidRoomType][templateName].append(room)
+                elif "!dual" in tags:
+                    templates[ROOM_TYPE.DUAL][templateName].append(room)
+                else:
+                    templates[ROOM_TYPE.NORMAL][templateName].append(room)
 
 #files = [f for f in listdir('.') if isfile(f) and ".lvl" in f]
 files = [f for f in listdir(mypath) if ".lvl" in f]
@@ -64,8 +87,8 @@ for filename in files:
     print("FILE ", filename)
     f = open(mypath + filename, "r", encoding="latin-1")
     lvl = f.read()
-    getTemplateRooms(lvl)
-    levels[filename] = lvl
+    getTemplateRooms(lvl, filename)
+    filesDict[filename] = lvl
     f.close()
 
 tryMakeDir("./Templates")
@@ -115,25 +138,36 @@ def changeLevelTemplate(level, templateName, roomsStr):
 
 tryMakeDir("./mixed")
 
-for index, value in templates.items():
-    print(index)
-
 def fixTemplates():
-    templates["exit"][:] = [room for room in templates["exit"] if "8" in room or "9" in room]
+    templates[ROOM_TYPE.NORMAL]["exit"][:] = [room for room in templates[ROOM_TYPE.NORMAL]["exit"] if "8" in room or "9" in room]
 fixTemplates()
 
-for levelName, level in levels.items():
-    if levelName == "tiamat.lvl":
+for filename, level in filesDict.items():
+    if filename == "tiamat.lvl":
         continue
-    if not ("\\.chunk_air" in level):
-        level += f"\n{templateSeparator}\n\\.chunk_air\n{templateSeparator}\n\n"
-    if not ("\\.chunk_ground" in level):
-        level += f"\n{templateSeparator}\n\\.chunk_ground\n{templateSeparator}\n\n"
-    if not ("\\.chunk_door" in level):
-        level += f"\n{templateSeparator}\n\\.chunk_door\n{templateSeparator}\n\n"
+    isSunkenLevel = "sunken" in filename or filename == "hundun.lvl"
+    if filename != "generic.lvl":
+        if not ("\\.chunk_air" in level):
+            level += f"\n{templateSeparator}\n\\.chunk_air\n{templateSeparator}\n\n"
+        if not ("\\.chunk_ground" in level):
+            level += f"\n{templateSeparator}\n\\.chunk_ground\n{templateSeparator}\n\n"
+        if not ("\\.chunk_door" in level):
+            level += f"\n{templateSeparator}\n\\.chunk_door\n{templateSeparator}\n\n"
     for templateName in toChangeTemplates:
-        level = changeLevelTemplate(level, templateName, "\n\n".join(templates[templateName]))
-    f = open("./mixed/" + levelName, "w", encoding="latin-1")
+        templateRooms = []
+        if templateName == "exit_notop" and isSunkenLevel:
+            templateRooms.extend(sunkenExitsNotop)
+        else:
+            templateRooms.extend(templates[ROOM_TYPE.NORMAL][templateName])
+        if isSunkenLevel:
+            templateRooms.extend(templates[ROOM_TYPE.INVERSE_LIQUID][templateName])
+        elif filename != "icecavesarea.lvl":
+            templateRooms.extend(templates[ROOM_TYPE.LIQUID][templateName])
+        
+        if not ("cosmicocean" in filename or "duat" in filename):
+            templateRooms.extend(templates[ROOM_TYPE.DUAL][templateName])
+        level = changeLevelTemplate(level, templateName, "\n\n".join(templateRooms))
+    f = open("./mixed/" + filename, "w", encoding="latin-1")
     f.write(level)
     f.close()
 
